bf94c96c3c6f9f8b5991ba7b4c196ce15fa1d29a
pjy0308 <84903333+pjy0308@users.noreply.github.com>
2023-07-24 21:35:19 +0900

    위치 권한 요청 구현 완료, 권한 거부될 경우 프로그램 동작 수정 필요 (현재는 한번 거부하면 지도 버튼 클릭 시 다시 요청 메세지는 안뜨고 바로 종료됨)

--- app/src/main/java/com/example/firstaidmaster/aedMap.kt
+++ app/src/main/java/com/example/firstaidmaster/aedMap.kt
@@ -1,5 +1,6 @@
 package com.example.firstaidmaster
 
+import android.app.AlertDialog
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,23 +16,41 @@ class aedMap : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var locationSource: FusedLocationSource    // 위치를 반환하는 구현체
     private lateinit var naverMap: NaverMap
 
+    private val REQUEST_LOCATION =100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aed_map)
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        // 앱 실행시 권한이 부여되지 않은 경우 요청
+        if (ActivityCompat.checkSelfPermission(this@aedMap, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this@aedMap, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            var permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                        android.Manifest.permission.ACCESS_COARSE_LOCATION)
+
+            ActivityCompat.requestPermissions(this@aedMap, permissions, REQUEST_LOCATION)
+        }
     }
 
+    // 권한을 거부했을 경우 앱을 종료
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray) {
-        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-            if (!locationSource.isActivated) {  // 권한 거부됨
-                naverMap.locationTrackingMode = LocationTrackingMode.None
-            }
-            return
-        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode === REQUEST_LOCATION) {
+            if (grantResults.size > 0) {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        System.exit(0)
+                    }
+                }
+            }
+        }
     }
 
     override fun onMapReady(naverMap: NaverMap) {
