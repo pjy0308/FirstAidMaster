9890996c7c07dc8a7385b1c7f190686afe7b2353
hyerim <hyerim46@swu.ac.kr>
2023-07-25 13:07:30 +0900

    하단바 추가 (화면 전환 이전)

--- app/src/main/java/com/example/firstaidmaster/MainActivity.kt
+++ app/src/main/java/com/example/firstaidmaster/MainActivity.kt
@@ -5,12 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.RelativeLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var FirstAidEducation: Button
     lateinit var FirstAidVideo: Button
     lateinit var HowToUseAED: Button
     lateinit var imageButton: ImageButton
+
+    private val frame: RelativeLayout by lazy { // activity_main의 화면 부분
+        findViewById(R.id.body_container)
+    }
+    private val bottomNagivationView: BottomNavigationView by lazy { // 하단 네비게이션 바
+        findViewById(R.id.bottom_navigation)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,5 +47,32 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, aedMap::class.java)
             startActivity(intent)
         }
+
+        // 애플리케이션 실행 후 첫 화면 설정
+        //supportFragmentManager.beginTransaction().add(frame.id, HomeFragment()).commit()
+
+        // 하단 네비게이션 바 클릭 이벤트 설정
+        bottomNagivationView.setOnNavigationItemSelectedListener {item ->
+            when(item.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.map -> {
+                    replaceFragment(MapFragment())
+                    true
+                }
+                R.id.setting -> {
+                    replaceFragment(SettingFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+    // 화면 전환 구현 메소드
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(frame.id, fragment).commit()
     }
 }
