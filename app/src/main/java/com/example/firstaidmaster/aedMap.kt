d295bb2966a8b2c3824ef7180b104d70c63408dc
pjy0308 <84903333+pjy0308@users.noreply.github.com>
2023-07-27 02:41:04 +0900

    현재 위치 구현하려 했으나 실패

--- app/src/main/java/com/example/firstaidmaster/aedMap.kt
+++ app/src/main/java/com/example/firstaidmaster/aedMap.kt
@@ -7,10 +7,13 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapOptions
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.util.FusedLocationSource
 
@@ -23,7 +26,7 @@ class aedMap : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aed_map)
-        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        //locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
         // 앱 실행시 권한이 부여되지 않은 경우 요청
         if (ActivityCompat.checkSelfPermission(this@aedMap, android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -44,26 +47,29 @@ class aedMap : AppCompatActivity(), OnMapReadyCallback {
         if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
             if (!locationSource.isActivated) {  // 권한 거부됨
                 naverMap.locationTrackingMode = LocationTrackingMode.None
+            } else {
+                naverMap.locationTrackingMode = LocationTrackingMode.Follow
             }
         }
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-//        if (requestCode === REQUEST_LOCATION) {
-//            if (grantResults.size > 0) {
-//                for (grant in grantResults) {
-//                    if (grant != PackageManager.PERMISSION_GRANTED) {
-//                        System.exit(0)
-//                    }
-//                }
-//            }
-//        }
     }
 
     override fun onMapReady(naverMap: NaverMap) {
         this.naverMap = naverMap
+
+        // 지도 초기 위치
+        val cameraUpdate: CameraUpdate = CameraUpdate.scrollTo(LatLng(37.6283, 127.0904))
+        naverMap.moveCamera(cameraUpdate)
+
+        // 현재위치 버튼
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = true
+
+        // 위치를 반환하는 FusedLocationSource 생성
+        locationSource = FusedLocationSource(this@aedMap, LOCATION_PERMISSION_REQUEST_CODE)
         naverMap.locationSource = locationSource
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        //naverMap.locationTrackingMode = LocationTrackingMode.Follow
     }
 
     companion object {
